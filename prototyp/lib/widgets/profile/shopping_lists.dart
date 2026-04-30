adccd2ff176f17e06b3eaa5ace2fe12761ef48a1
Love Lysell Berglund <love.lysell.berglund@gmail.com>
2026-04-30 15:26:40 +0200

    wip: shopping lists

---
+++ prototyp/lib/widgets/profile/shopping_lists.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:prototyp/widgets/titled_box.dart';
+
+class ShoppingLists extends StatelessWidget {
+  const ShoppingLists({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const TitledBox(title: "Dina inköpslistor", child: Placeholder());
+  }
+}
