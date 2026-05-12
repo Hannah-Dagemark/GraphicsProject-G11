0e7a6238b336fcaa449cf97ed1eaf805bf87cf67
Hannah dagemark <dagemark@student.chalmers.se>
2026-05-12 15:18:45 +0200

    Refactored CategoryItems into global widget ProductGrid

--- prototyp/lib/widgets/product_grid.dart
+++ prototyp/lib/widgets/product_grid.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:prototyp/model/browsecategory_model.dart';
 import 'package:prototyp/model/imat/imat_data_handler.dart';
-import 'package:prototyp/widgets/browse/category_item.dart';
+import 'package:prototyp/widgets/product_card.dart';
 import 'package:provider/provider.dart';
 
-class CategoryItems extends StatelessWidget {
-  const CategoryItems({super.key});
+class ProductGrid extends StatelessWidget {
+  final bool filterProducts;
+
+  const ProductGrid({super.key, this.filterProducts = false});
 
   @override
   Widget build(BuildContext context) {
@@ -16,16 +18,24 @@ class CategoryItems extends StatelessWidget {
       spacing: 20,
       runSpacing: 20,
       children: [
-        if (browsecategoryModel.currentSearch.isEmpty) ...[
+        if (browsecategoryModel.currentSearch.isEmpty || !filterProducts) ...[
           for (final product in imatDataHandler.findProductsByCategory(
             browsecategoryModel.currentCategory,
           ))
-            CategoryItem(product: product),
+            SizedBox(
+              width: 200,
+              height: 300,
+              child: ProductCard(product, imatDataHandler),
+            ),
         ] else ...[
           for (final product in imatDataHandler.findProducts(
             browsecategoryModel.currentSearch,
           ))
-            CategoryItem(product: product),
+            SizedBox(
+              width: 200,
+              height: 300,
+              child: ProductCard(product, imatDataHandler),
+            ),
         ],
       ],
     );
